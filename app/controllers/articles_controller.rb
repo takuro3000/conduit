dff35fa7477b2e9c8a324d1ee5cee0a44817d11d
takuro <talangxiaotian6@gmail.com>
2024-04-10 11:31:51 +0900

    add articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,15 @@ class ArticlesController < ApplicationController
     end
     def new
     end
+    def create
+        @article=Article.new(
+            title: params[:title],
+            summary: params[:summary],
+            content: params[:content]
+        )
+        @article.save
+        @genre=Genre.new(name: params[:name])
+        @genre.save
+        redirect_to("/articles/home")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "articles/home" => "articles#index"
   get "articles/1" => "articles#show"
   get "articles/new" => "articles#new"
+  post "articles/create" => "articles#create"
 end
