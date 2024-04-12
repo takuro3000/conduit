86c102e1f1da7ab26b11599231a5653a15d421fd
takuro <talangxiaotian6@gmail.com>
2024-04-12 17:37:35 +0900

    change header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,18 @@ class UsersController < ApplicationController
     def login
         @user=User.find_by(email: params[:email])
         if @user
+            session[:user_id] = @user.id
             redirect_to("/articles/home")
         else
             @error_message = "This email or password is incorrect"
             @email=params[:email]
             @password=params[:password]
             render("users/login_form")
+        end
+    end
+    def ensure_correct_user
+        if @current_user.id != params[:id].to_i
+            redirect_to("/articles/home")
         end
     end
 end
