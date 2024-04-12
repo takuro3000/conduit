3289ec305e2f9bdc1a3194452501a38ec2e80abe
takuro <talangxiaotian6@gmail.com>
2024-04-12 12:26:03 +0900

    add validation in new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
         @article=Article.find_by(id: params[:id])
     end
     def new
+        @article=Article.new
     end
     def create
         @article=Article.new(
@@ -13,13 +14,16 @@ class ArticlesController < ApplicationController
             summary: params[:summary],
             content: params[:content]
         )
-        @article.save
         tags = params[:tags].split(",").map(&:strip)
         tags.each do |tag_name|
             tag = Tag.find_or_create_by(name: tag_name)
             @article.tags << tag
         end
-        redirect_to("/articles/home")
+        if @article.save
+            redirect_to("/articles/home")
+        else
+            render("articles/new")
+        end
     end
     def edit
         @article=Article.find_by(id: params[:id])
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
     has_and_belongs_to_many :tags
+    validates :title, {presence: true, length: {maximum: 30}}
 end
